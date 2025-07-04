453a9a4749bd0ce739bf45e8d737f38d828e9755
HardMain <vova68362@gmail.com>
2025-07-04 17:10:55 +0500

    Connecting ef core

---
+++ backend/src/PetFamily.Domain/Volunteers/ValueObjects/SocialNetwork.cs
@@ -0,0 +1,29 @@
+﻿using PetFamily.Domain.Shared.Entities;
+
+namespace PetFamily.Domain.Volunteers.ValueObjects
+{
+    public record SocialNetwork
+    {
+        private SocialNetwork() { }
+
+        private SocialNetwork(string url, string platform)
+        {
+            URL = url;
+            Platform = platform;
+        }
+
+        public string URL { get; } = default!;
+        public string Platform { get; } = default!;
+
+        public static Result<SocialNetwork> Create(string url, string platform)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                return "Url can not be empty!";
+
+            if (string.IsNullOrWhiteSpace(platform))
+                return "Platform can not be empty!";
+
+            return new SocialNetwork(url, platform);
+        }
+    }
+}
---
+++ backend/src/PetFamily.Infrastructure/Configurations/BreedConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.Shared.Constants;
+using PetFamily.Domain.Species.Entities;
+using PetFamily.Domain.Species.ValueObjects;
+
+namespace PetFamily.Infrastructure.Configurations
+{
+    public class BreedConfiguration : IEntityTypeConfiguration<Breed>
+    {
+        public void Configure(EntityTypeBuilder<Breed> builder)
+        {
+            builder.ToTable("breeds");
+
+            builder.HasKey(b => b.Id)
+                .HasName("pk_breeds");
+
+            builder.Property(b => b.Id)
+                .IsRequired()
+                .HasConversion(
+                    id => id.Value, 
+                    value => BreedId.Create(value)
+                )
+                .HasColumnName("id");
+
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH)
+                .HasColumnName("name");
+        }
+    }
+}
